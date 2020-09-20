a93b44dd1958a5bf68c25a89bcb27b89a4ed2f0e
Luiz Henrique Laurini <luizhenriquelaurini@gmail.com>
2020-09-20 19:02:38 -0300

    Change CSR and Apple battery level formulas

--- hsphfpd.pl
+++ hsphfpd.pl
@@ -1271,7 +1271,7 @@ sub hsphfpd_csr_battery_level_changed {
 	print "CSR battery level changed\n";
 	# CSR battery level is only in range 0-9 but HF battery level is range 0-100, so prefer usage of HF
 	return if exists $endpoints{$endpoint}->{hf_indicators}->{'battery-level'};
-	$endpoints{$endpoint}->{properties}->{BatteryLevel} = dbus_int16(($level >= 0 && $level <= 9) ? ($level * 100 / 9) : -1);
+	$endpoints{$endpoint}->{properties}->{BatteryLevel} = dbus_int16(($level >= 0 && $level <= 9) ? (($level + 1) * 10) : -1);
 	$endpoints{$endpoint}->{object}->emit_signal('PropertiesChanged', 'org.hsphfpd.Endpoint', { BatteryLevel => $endpoints{$endpoint}->{properties}->{BatteryLevel} }, []);
 }
 
@@ -1543,7 +1543,7 @@ sub hsphfpd_socket_ready_read {
 							print "Apple battery level changed\n";
 							# Apple battery level is only in range 0-9 but HF battery level is range 0-100, so prefer usage of HF
 							if (not exists $endpoints{$endpoint}->{hf_indicators}->{'battery-level'}) {
-								$endpoints{$endpoint}->{properties}->{BatteryLevel} = dbus_int16(($val >= 0 && $val <= 9) ? ($val * 100 / 9) : -1);
+								$endpoints{$endpoint}->{properties}->{BatteryLevel} = dbus_int16(($val >= 0 && $val <= 9) ? (($val + 1) * 10) : -1);
 								$endpoints{$endpoint}->{object}->emit_signal('PropertiesChanged', 'org.hsphfpd.Endpoint', { BatteryLevel => $endpoints{$endpoint}->{properties}->{BatteryLevel} }, []);
 							}
 						} elsif ($key == 2 and $val >= 0 and $val <= 1) {
@@ -1808,7 +1808,7 @@ sub hsphfpd_socket_ready_read {
 							print "Apple battery level changed\n";
 							# Apple battery level is only in range 0-9 but HF battery level is range 0-100, so prefer usage of HF
 							if (not exists $endpoints{$endpoint}->{hf_indicators}->{'battery-level'}) {
-								$endpoints{$endpoint}->{properties}->{BatteryLevel} = dbus_int16(($val >= 0 && $val <= 9) ? ($val * 100 / 9) : -1);
+								$endpoints{$endpoint}->{properties}->{BatteryLevel} = dbus_int16(($val >= 0 && $val <= 9) ? (($val + 1) * 10) : -1);
 								$endpoints{$endpoint}->{object}->emit_signal('PropertiesChanged', 'org.hsphfpd.Endpoint', { BatteryLevel => $endpoints{$endpoint}->{properties}->{BatteryLevel} }, []);
 							}
 						} elsif ($key == 2 and $val >= 0 and $val <= 1) {
